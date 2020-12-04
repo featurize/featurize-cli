6e22b3d82c43ec2cd58d931cf144815e64d96a74
chenglu <chenglu.she@gmail.com>
2020-12-04 19:02:16 +0800

    change version

--- featurize/__init__.py
+++ featurize/__init__.py
@@ -1,4 +1,4 @@
 from .featurize_client import FeaturizeClient
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 __all__ = ['FeaturizeClient']
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(requirements_file, 'r') as f:
 
 setuptools.setup(
     name='featurize',
-    version='0.0.8',
+    version='0.0.9',
     author='chenglu',
     description='',
     packages=setuptools.find_packages(),

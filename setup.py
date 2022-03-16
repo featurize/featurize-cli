0dbad9b36f0ba20662a1064c6d62ab2bf08ddfac
chenglu <chenglu.she@gmail.com>
2022-03-16 14:21:02 +0800

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(requirements_file, 'r') as f:
 
 setuptools.setup(
     name='featurize',
-    version='0.0.21',
+    version='0.0.22',
     author='chenglu',
     description='',
     packages=setuptools.find_packages(),
